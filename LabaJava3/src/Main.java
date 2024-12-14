977e960f337429345564f75c190c0e4d7a9040c9
Nasyrov Dmytro <rentop.pvp.yt@gmail.com>
2024-12-14 19:20:04 +0200

    Second and final step of refactoring this file Decided not to split into more steps, because while doing one principal others went really fluently into them Explained a bit of logic behind in the comments

--- LabaJava3/src/Main.java
+++ LabaJava3/src/Main.java
@@ -3,87 +3,169 @@ package com.gildedrose;
 * else замінив на виходи через return
 * спробував дотримуватися максимум одного відступу
 * але деінде не вийшло, далі поправлю, сподіваюся
+*
+*
+*
+* Хотів розбити для ясності на три етапи задля ясності процесу
+* але під час Wrapping'у все якось встало на свої місця одразу
+* й не бачив сенсу якісь проміжкові етапи робити
+* Цю ідею з Wrapper`ами підглянув у розумних людей на StackOverflow:)
+*
+* Всі принципи вже, ніби, враховані в цей раз
 * */
 class GildedRose {
-    Item[] items;
+    private final ItemCollection items;
 
     public GildedRose(Item[] items) {
-        this.items = items;
+        this.items = new ItemCollection(items);
     }
 
     public void updateQuality() {
-        for (Item item : items) {
-            if (isLegendary(item)) {
-                continue;
-            }
+        items.updateQualityForAll();
+    }
+}
 
-            updateSellIn(item);
+class ItemCollection {
+    private final ItemWrapper[] items;
 
-            if (isSpecialItem(item)) {
-                updateSpecialItemQuality(item);
-                continue;
-            }
+    public ItemCollection(Item[] items) {
+        this.items = wrapItems(items);
+    }
 
-            degradeQuality(item);
+    private ItemWrapper[] wrapItems(Item[] items) {
+        ItemWrapper[] wrappedItems = new ItemWrapper[items.length];
+        for (int i = 0; i < items.length; i++) {
+            wrappedItems[i] = ItemWrapperFactory.create(items[i]);
+        }
+        return wrappedItems;
+    }
 
-            if (isExpired(item)) {
-                handleExpiredItem(item);
-            }
+    public void updateQualityForAll() {
+        for (ItemWrapper item : items) {
+            item.update();
+        }
+    }
+}
+/*Основна ідея полягає в тому, що ми опрацьовуємо кожен тип предмету по своєму
+ * та застосовуємо унікальні способи опрацювання через Override до кожного
+ * Я не впевнений, що зберіглася на 100% логіка, але ніби, все повинно було
+ * залишитися так, як і було
+ */
+abstract class ItemWrapper {
+    protected final Item item;
+
+    public ItemWrapper(Item item) {
+        this.item = item;
+    }
+
+    public final void update() {
+        if (isSulfuras()) {
+            return;
+        }
+        updateSellIn();
+        updateQuality();
+        if (isExpired()) {
+            handleExpiration();
         }
     }
 
-    private boolean isLegendary(Item item) {
+    protected boolean isSulfuras() {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
 
-    private void updateSellIn(Item item) {
+    protected void updateSellIn() {
         item.sellIn -= 1;
     }
 
-    private boolean isSpecialItem(Item item) {
-        return item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    protected abstract void updateQuality();
+
+    protected boolean isExpired() {
+        return item.sellIn < 0;
     }
 
-    private void updateSpecialItemQuality(Item item) {
-        increaseQuality(item);
+    protected abstract void handleExpiration();
+}
+//тут super, бо йде до батьківського класу
+class AgedBrieWrapper extends ItemWrapper {
+    public AgedBrieWrapper(Item item) {
+        super(item);
+    }
 
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.sellIn < 10) {
-                increaseQuality(item);
-            }
-            if (item.sellIn < 5) {
-                increaseQuality(item);
-            }
+    @Override
+    protected void updateQuality() {
+        increaseQuality();
+    }
+
+    @Override
+    protected void handleExpiration() {
+        increaseQuality();
+    }
+
+    private void increaseQuality() {
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
+    }
+}
+
+class BackstagePassWrapper extends ItemWrapper {
+    public BackstagePassWrapper(Item item) {
+        super(item);
+    }
+
+    @Override
+    protected void updateQuality() {
+        increaseQuality();
+        if (item.sellIn < 10) {
+            increaseQuality();
+        }
+        if (item.sellIn < 5) {
+            increaseQuality();
         }
     }
 
-    private void degradeQuality(Item item) {
+    @Override
+    protected void handleExpiration() {
+        item.quality = 0;
+    }
+
+    private void increaseQuality() {
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
+    }
+}
+
+class StandardItemWrapper extends ItemWrapper {
+    public StandardItemWrapper(Item item) {
+        super(item);
+    }
+
+    @Override
+    protected void updateQuality() {
+        decreaseQuality();
+    }
+
+    @Override
+    protected void handleExpiration() {
+        decreaseQuality();
+    }
+
+    private void decreaseQuality() {
         if (item.quality > 0) {
             item.quality -= 1;
         }
     }
+}
 
-    private boolean isExpired(Item item) {
-        return item.sellIn < 0;
-    }
-
-    private void handleExpiredItem(Item item) {
+class ItemWrapperFactory {
+    public static ItemWrapper create(Item item) {
         if (item.name.equals("Aged Brie")) {
-            increaseQuality(item);
-            return;
+            return new AgedBrieWrapper(item);
         }
-
         if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            item.quality = 0;
-            return;
+            return new BackstagePassWrapper(item);
         }
-
-        degradeQuality(item);
-    }
-
-    private void increaseQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality += 1;
-        }
+        return new StandardItemWrapper(item);
     }
 }
