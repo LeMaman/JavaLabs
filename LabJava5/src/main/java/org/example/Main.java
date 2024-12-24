76f801ade298d62a0ae0b6f8e50ffe5ef2303ca1
Nasyrov Dmytro <rentop.pvp.yt@gmail.com>
2024-12-24 04:39:41 +0200

    First commit of Lab5. Created what I expect my future program to be. Next step - writing test over it

---
+++ LabJava5/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class ChatApplication {
+    public static void main(String[] args) {
+        ChatMediator mediator = new ChatMediatorImpl();
+
+        User user1 = new UserImpl(mediator, "Alice");
+        User user2 = new UserImpl(mediator, "Bob");
+        User user3 = new UserImpl(mediator, "Charlie");
+
+        mediator.addUser(user1);
+        mediator.addUser(user2);
+        mediator.addUser(user3);
+
+        Message message = new BasicMessage("Hello, everyone!");
+        message = new EncryptedMessageDecorator(message);
+        message = new EmojiMessageDecorator(message);
+
+        user1.sendMessage(message.getContent());
+    }
+}
